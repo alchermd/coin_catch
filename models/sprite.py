bbc011444f6627c96e569e9f579f2e18ebe18d12
alchermd <johnalcherdoloiras@gmail.com>
2017-09-30 19:19:02 +0800

    Paddle can now be controlled with the LEFT and RIGHT buttons.

--- models/sprite.py
+++ models/sprite.py
@@ -8,14 +8,16 @@ class Paddle(pygame.sprite.Sprite):
     """
     A player-controlled paddle.
     """
-    def __init__(self, img: pygame.Surface):
+    def __init__(self, img: pygame.Surface, screen: pygame.Surface):
         """
         Creates a new Paddle object.
 
         Args:
             img: A pygame.Surface image loaded through pygame.image.load
+            screen: A pygame.Surface where the Paddle is to be drawn.
         """
         super().__init__()
+        self.screen = screen
 
         # Create the Paddle surface.
         width, height = img.get_width(), img.get_height()
@@ -24,3 +26,15 @@ class Paddle(pygame.sprite.Sprite):
 
         # Save the dimensions to the rect attribute.
         self.rect = self.image.get_rect()
+
+        self.x_speed = 0
+
+
+    def update(self):
+        self.rect.x += self.x_speed
+
+        # Prevent going out of bounds.
+        if self.rect.x <= 0:
+            self.rect.x = 0
+        elif self.rect.x >= self.screen.get_width() - self.image.get_width():
+            self.rect.x = self.screen.get_width() - self.image.get_width()
